414dfd53d1d4495480c0bf6336cb950702b356b4
Jgreen42 <creativemind47@outlook.com>
2022-04-01 17:09:36 -0500

    ascii conversion working attatching event attributes and api

--- public/app.js
+++ public/app.js
@@ -1,15 +1,6 @@
-
-const form = document.querySelector(".user-form");
-
-form.addEventListener("submit", (e) => {
-    const data = new FormData(e.target);
-    const newUser = {
-        name: data.get("name"),
-        photoid: data.get("photoid"),
-    };
-    $.post({
-        url: "/users" 
-
-
-        
-       
+let btn = document.createElement("button");
+btn.innerHTML = "Save";
+btn.addEventListener("click", function () {
+  alert("Button is clicked");
+});
+document.body.appendChild(btn);
