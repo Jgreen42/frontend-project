78161628cba8c0deefe2784ba1e17d96df97ebe3
Jgreen42 <creativemind47@outlook.com>
2022-04-04 19:10:42 -0500

    file changes and messing with functionality, added cut out effect

--- public/app.js
+++ public/app.js
@@ -1,6 +1,50 @@
-let btn = document.createElement("button");
-btn.innerHTML = "Save";
-btn.addEventListener("click", function () {
-  alert("Button is clicked");
-});
-document.body.appendChild(btn);
+
+
+function download() {
+  var save = document.getElementById("example").value;
+  var blob = new Blob([save], {
+    type: "text/plain;charset=utf-8"
+  });
+  saveAs(blob, "yournewfile.txt");
+};
+
+function storageAvailable(type) {
+  var storage;
+  try {
+      storage = window[type];
+      var x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+  }
+  catch(e) {
+      return e instanceof DOMException && (
+          // everything except Firefox
+          e.code === 22 ||
+          // Firefox
+          e.code === 1014 ||
+          // test name field too, because code might not be present
+          // everything except Firefox
+          e.name === 'QuotaExceededError' ||
+          // Firefox
+          e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+          // acknowledge QuotaExceededError only if there's something already stored
+          (storage && storage.length !== 0);
+
+          
+  }
+  
+}
+if (storageAvailable('localStorage')) {
+  console.log("local storage is usable @(0_~)@");
+  // Yippee! We can use localStorage awesomeness
+}
+else {
+ alert("local storage is full un able to convert!")
+  console.log("local storage is full!")
+};
+
+
+    
+  
+
