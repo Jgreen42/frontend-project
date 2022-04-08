cae92482966286f279a36ee04fffa7efd4bcaaa8
Jgreen42 <creativemind47@outlook.com>
2022-04-08 01:02:08 -0500

    troubleshooting

--- db/migration.sql
+++ db/migration.sql
@@ -1,13 +1,11 @@
 
-DROP TABLE IF EXISTS users;
-
 CREATE TABLE users (
 id SERIAL PRIMARY KEY,
 name TEXT NOT NULL,
 photoid INTEGER NOT NULL,
 );
 
-INSERT INTO users(name, photoid) VALUES ('bubbles', 1);
-INSERT INTO users(name, photoid) VALUES ('julian', 2);
-INSERT INTO users(name, photoid) VALUES ('rickys', 3);
-INSERT INTO users(name, photoid) VALUES ('conky', 4);
+INSERT INTO users(name, photoid) VALUES ('', 1);
+INSERT INTO users(name, photoid) VALUES ('', 2);
+INSERT INTO users(name, photoid) VALUES ('', 3);
+INSERT INTO users(name, photoid) VALUES ('', 4);
