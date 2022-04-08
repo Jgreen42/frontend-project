f90b67efd963c2daedcb8e41647b1eed0117a985
Jgreen42 <creativemind47@outlook.com>
2022-04-08 10:30:28 -0500

    configured files and links/ spelling errors

--- conn.js
+++ conn.js
@@ -3,6 +3,9 @@ const { pool } = require("pg");
 const pool = new Pool({
   // Format: postgres://user:password@host:5432/database
     connectionString: process.env.DATABASE_URL,
+    ssl:{
+      rejectUnauthorized:false
+    }
   });
 
   module.exports = pool;
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 const express = require("express");
-const db = require("./db/conn");
+const db = require("./conn");
 const app = express();
 
 app.use(express.static("public"));
 console.log(process.env);
 
 app.get("/api/users", (_, res) => {
-  DB.query("SELECT * FROM users").then((data) => {
+  db.query("SELECT * FROM users").then((data) => {
     res.json(data.rows);
   });
 });
