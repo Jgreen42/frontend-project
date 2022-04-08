d4a1eade7384441bcb02e9e7ac3b9408242ebb84
Jgreen42 <creativemind47@outlook.com>
2022-04-08 18:04:35 -0500

    commit prior to file manipulation and prevent loss from possible data loss

--- public/app.js
+++ public/app.js
@@ -1,29 +1,6 @@
-document.querySelector("#alpha_myFile").addEventListener("change", function () {
-  const reader = new FileReader();
-
-  reader.addEventListener("load", () => {
-    localStorage.setItem("recent-image", reader.result);
-  });
-
-  reader.readAsDataURL(this.files[0]);
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  const recentImagDataUrl = localStorage.getItem("recent-image");
-
-  if (recentImagDataUrl) {
-    document.querySelector("#content").setAttribute("src", recentImagDataUrl) &&
-      document
-        .querySelector("#loadImage")
-        .setAttribute("src", recentImagDataUrl);
-  }
-  document.addEventListener("SubmitEvent", (event) => {
-    if (event) {
-      const reader2 = new FileReader();
-
-      reader2.addEventListener("load", () => {
-        localStorage.setItem("recent-image", reader.result);
-      });
-    }
-  });
+document.querySelector(".imgGrab").addEventListener("submit", (e)=> {
+    e.preventDefault()
+    console.log("working");
+    const data = new FormData(e.target);
+  console.log('data rendered', data);
 });
